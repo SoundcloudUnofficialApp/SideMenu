8ba57d9aee9a5cd61063f410cc008cdeeec1064a
Oleksandr Bretsko <bretsko.oleksandr@gmail.com>
2019-09-01 11:21:39 +0300

    converted to Swift Package

---
+++ SideMenuExample/SideMenuExample/Common/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  SideMenuExample
+//
+//  Created by Oleksandr Bretsko on 9/1/19.
+//  Copyright © 2019 Oleksandr Bretsko. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
